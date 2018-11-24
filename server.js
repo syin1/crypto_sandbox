dc00bfe3dc78d54a63556d316bb30f7e4692b226
syin <shaomingyinut@gmail.com>
2018-11-24 13:10:11 -0500

    public to build folder

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ server.use('/api/v1', router);
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(path.join(__dirname, 'build/')));
+  server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'build/index.html'));
+  });
 } else {
   server.use(express.static(path.join(__dirname, 'public/')));
+  server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'public/index.html'));
+  });
 }
-
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
-});
 
 server.listen(PORT, function() {
   console.log(`API Server now listening on PORT ${PORT}`);
